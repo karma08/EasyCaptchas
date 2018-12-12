0941d17a0490cd08fa934f72b225a80a400c76b8
karma08 <45629296+karma08@users.noreply.github.com>
2018-12-12 14:31:11 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,15 @@
 <?php
+<html>
+<head>
+<meta charset="UTF-8" />
+</head>
+<body>
 
 /* SCRIPT FRONTAL AFFICHAGE DE CAPTCHA
-* AFFICHER UNE IMAGE GÉNÉRÉE EN PHP */
+* AFFICHER UNE IMAGE GÃ‰NÃ‰RÃ‰E EN PHP */
 
 echo "<img src='script-captchas.php' alt='captchas' />";
-
+</body>
+</html>
 ?>
+
