584f040c44ef54642cf4ade3da4072157e59674e
karma08 <45629296+karma08@users.noreply.github.com>
2018-12-12 14:40:03 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,12 @@
 </head>
 <body>
 
+
+  <form method="post" action="">
+      <input type="text" id="text" value="text" />
+      <input type="submit" id="valider" value="valider" />
+  </form>
+  
 /* SCRIPT FRONTAL AFFICHAGE DE CAPTCHA
 * AFFICHER UNE IMAGE GÉNÉRÉE EN PHP */
 
